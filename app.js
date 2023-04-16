8b7237cc925725b08a9f918fb279d1a5699642ed
chandan varma <varmachandan8080@gmail.com>
2023-04-16 23:22:02 +0530

    file is created

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require ('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+res.write(' this is home page ');
+    res.send();
+});
+app.get('/courses',(req,res)=>{
+    res.send(courses);
+});
+const port =process.env.PORT || 3000;
+//const port =5000;
+//const port =process.env.PORT || 5000 ;
+app.listen(port,()=>{console.log("listning  on port ${port}...");
+});
